508fdc0df17d7893e03eca5006a625986b64be7a
unknown <dethgrenade@gmail.com>
2020-08-10 13:21:03 -0700

    WiP day 1 - bfs completed

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -39,25 +39,20 @@ class Graph:
         """
         # Create an empty queue
         q = Queue()
-
         # Add starting vertex ID
         q.enqueue(starting_vertex)
-
         # Create set for visited verts
         visited = set()
-
         # While queue is not empty
         while q.size() > 0:
-
             # Dequeue a vert
             v = q.dequeue()
-
             # If not visited
             if v not in visited:
-
+                # Visit it!
+                print(v)
                 # Mark as visited
                 visited.add(v)
-
                 # Add all neighbors to the queue
                 for neighbor in self.get_neighbors(v):
                     q.enqueue(neighbor)
@@ -69,25 +64,20 @@ class Graph:
         """
         # Create an empty stack
         s = Stack()
-
         # Add starting vertex ID
         s.push(starting_vertex)
-
         # Create set for visited verts
         visited = set()
-
         # While stack is not empty
         while s.size() > 0:
-
             # pop a vert
             v = s.pop()
-
             # If not visited
             if v not in visited:
-
+                # Visit it!
+                print(v)
                 # Mark as visited
                 visited.add(v)
-
                 # Add all neighbors to the queue
                 for neighbor in self.get_neighbors(v):
                     s.push(neighbor)
@@ -107,7 +97,36 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        q = Queue()
+
+        q.enqueue([starting_vertex])
+        # Create a Set to store visited vertices
+        visited = set()
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first PATH
+            v = q.dequeue()
+            # Grab the last vertex from the PATH
+            last = v[-1]
+            # If that vertex has not been visited...
+            if last not in visited:
+                # CHECK IF IT'S THE TARGET
+                if last == destination_vertex:
+                    # IF SO, RETURN PATH
+                    return v
+            # Mark it as visited...
+            visited.add(last)
+            # Then add A PATH TO its neighbors to the back of the queue
+            for neighbor in self.get_neighbors(last):
+                # COPY THE PATH
+                path = v.copy()
+                # APPEND THE NEIGHOR TO THE BACK
+                path.append(neighbor)
+                # ADD the new path to the queue
+                q.enqueue(path)
+
+        return None
 
     def dfs(self, starting_vertex, destination_vertex):
         """
