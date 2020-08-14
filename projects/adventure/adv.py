d5a63e8d9b7d67e3c1e5d36e680302b22b6081be
unknown <dethgrenade@gmail.com>
2020-08-14 08:02:00 -0700

    added get_path() to find the closest path back to a room with an unexplored exit

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,15 +5,17 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack, Queue
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -42,26 +44,126 @@ maze_map = {}
 opp_dir = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
 
-def traverse_maze(prev_room=None, dir_moved=None):
-    # get current room number
-    room = player.current_room.id
-    # check map to see if we've been in this room before
-    if room not in maze_map:
-        # add room to map and init the exits for this room as none
-        maze_map[room] = {'n': None, 'e': None, 's': None, 'w': None}
-    # get the exits to this room
-    exits = player.current_room.get_exits()
-    # update the exits for the current room in the map
-    for e in exits:
-        if maze_map[room][e] is None:
-            maze_map[room][e] = '?'
-    # pick a direction to travel
-    print(f"Map Rooms: {maze_map}")
-    # travel that way.
-    # update map with previous room and direction travelled
+# get the shortest path from a deadend to a room with an unexplored exit
+def get_path(start):
+    # list to store the path to backtrack
+    route = []
+    # create a queue to hold paths for a BFS
+    q = Queue()
+    q.enqueue([start])
+    # set to keep track of rooms we have already visited
+    visited = set()
+
+    while q.size() > 0:
+        # get path from front of queue
+        path = q.dequeue()
+        # get the last room in the path to check adjacent rooms
+        last = path[-1]
+        # if we haven't visited this room yet
+        if last not in visited:
+            # check to see if its a room with unexplored exits
+            if '?' in maze_map[last].values():
+                # if it is then we need to convert the rooms in the path
+                # to directions to travel in order to get there
+                for i in range(len(path)-1):
+                    for k, v in maze_map[path[i]].items():
+                        if v == path[i+1]:
+                            route.append(k)
+                # return the path
+                return route
+        # mark room as visited
+        visited.add(last)
+
+        # look at the current room and add any explored neighbors to the search
+        for k, v in maze_map[last].items():
+            # copy the current path up to this point
+            new_path = path[:]
+            # add the next room to search to the new path
+            new_path.append(v)
+            # add the new path to our queue
+            q.enqueue(new_path)
+
+
+def traverse_maze():
+    # keep track of the number of unexplored exits we encounter
+    unexplored = 0
+    prev_room = None
+    dir_moved = None
+    backtracked = False
+    # init the list to hold the path through rooms
+    path = []
+    # create a stack to keep track of our path
+    s = Stack()
+    # init stack with the path
+    s.push(path)
+
+    while s.size() > 0:
+        # get the current room
+        room = player.current_room.id
+        # get the exits for the current room
+        exits = player.current_room.get_exits()
+
+        # if the current room is not in the map record it
+        if room not in maze_map:
+            # init the current room exits to None
+            maze_map[room] = {}
+
+        # update the current rooms exits
+        for e in exits:
+            # if the exit isn't marked
+            if e not in maze_map[room]:
+                # set it as '?'
+                maze_map[room][e] = '?'
+                # increment the unexplored exits
+                unexplored += 1
+            # if we made a move check if the rooms are marked as connected
+        for e in exits:
+            if prev_room is not None and prev_room in maze_map:
+                # if not already marked as connected record that in maze map
+                if maze_map[room][opp_dir[dir_moved]] == '?':
+                    maze_map[prev_room][dir_moved] = room
+                    maze_map[room][opp_dir[dir_moved]] = prev_room
+                    # decrement the unexplored exits
+                    unexplored -= 2
+            # if we haven't yet explored this way do so
+        for e in exits:
+            if maze_map[room][e] == '?':
+                # add the direction of the exit to the stack
+                s.push(e)
+        # break out of the loop once every room has been visited
+        if unexplored == 0:
+            break
+
+        # check to see if this room is a deadend and backtrack
+        # to nearest room with an unexplored exit if so
+        deadend = [v for k, v in maze_map[room].items() if v == '?']
+        if '?' not in deadend:
+            backtrack = get_path(room)
+            traversal_path.extend(backtrack)
+            for direction in backtrack:
+                player.travel(direction)
+
+        if backtracked:
+            backtracked = False
+            room = player.current_room.id
+
+        # grab the path off the top of the stack
+        path = s.pop()
+        # check to see if the path has directions
+        if len(path) > 0:
+            # store current room as the prev room
+            # (it will be prev after travel)
+            prev_room = player.current_room.id
+            # get direction of travel
+            dir_moved = direction = path[0]
+            # move one room in direction
+            player.travel(direction)
+            # record movement in traversal path
+            traversal_path.append(direction)
 
 
 traverse_maze()
+
 
 ##############################################
 
